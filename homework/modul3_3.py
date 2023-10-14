0a90e26b082f0fd966edb55f5e970ba4f9426cee
Lianoy <147326101+Lianoy@users.noreply.github.com>
2023-10-14 15:12:13 +0300

    Update modul3_3.py

--- homework/modul3_3.py
+++ homework/modul3_3.py
@@ -30,22 +30,42 @@ for i in m:
 print (short_words)
 
 ## Task #3
+def comparision (a, b): ## найти "большее" число
+    if a == b:
+        return a
+    if int(str(a)[0]) > int(str(b)[0]):
+        return a
+    elif int(str(a)[0]) < int(str(b)[0]):
+        return b
+    else: ##первая цифра в числах одинаковая
+        if a < 10 or b <10:
+            if a > b:
+                return b
+            else:
+                return a
+        else:
+            if int(str(a)[1]) > int(str(b)[1]):
+                return a
+            else:
+                return b
+
 def maximum (list):
-    max2 = ''
-    for i in list:
-        
-        
+    x = list[0]
+    for k in list:
+        x = comparision (x, k)           
+    return x      
     
 from random import randint
-m = [randint(0,100) for i in range(7)]
+n=7
+m = [randint(0,99) for i in range(n)]
 print (m)
 max1 = ''
-# for i in m:
-#     max1 += comparision
-i = 0
-for val1 in m:
-    for val2 in m[i:len(m)]:
-        if val1 > val2:
-            max1 += str(val1)
-    i += 1
+left = []
+for i in m:
+    left.append(i)
+
+for i in range(n):
+     max2 = maximum (left) ##ищем наибольшее из оставшихся чисел
+     max1 += str (max2)
+     left.remove (max2)
 print (max1)
